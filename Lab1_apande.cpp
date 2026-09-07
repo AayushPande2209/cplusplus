f61a686fd3d91e025b91e5c0d625b10898b746ec
Aayush <aayushpande.220@gmail.com>
2026-09-07 17:44:09 -0400

    Lab 1 - Welcome Greeting

---
+++ Lab1_apande.cpp
@@ -0,0 +1,22 @@
+/**
+ * @file Lab1_username.cpp
+ * @author Aayush Pande
+ * @date 2026-09-07
+ * @brief A simple program to greet the user by name.
+ */
+
+#include <iostream>
+
+int main() {
+    std::cout << "Enter your first name: ";
+    std::string fName;
+    std::cin >> fName;
+
+    std::cout << "Enter your last name: ";
+    std::string lName;
+    std::cin >> lName;
+
+    std::cout << "Welcome to C++ Programming, " << fName << " " << lName << "!";
+
+    return 0;
+}
